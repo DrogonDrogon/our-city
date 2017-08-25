7f44056a9ea1fd54fda3cd00313459ac2231c215
Nick <nicholaspowel@users.noreply.github.com>
2017-08-25 17:22:38 -0400

    further login tweaks

--- components/AppRoot.js
+++ components/AppRoot.js
@@ -5,33 +5,7 @@ import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from 'redux-logger';
 import reducer from '../redux/reducer';
 import RootNavigation from './RootNavigation';
-import LoginScreen from '../screens/LoginScreen'
-
-// Listen for authentication state to change.
-firebase.auth().onAuthStateChanged((user) => {
-  if (user != null) {
-    console.log("We are authenticated now!");
-  }
-
-  // Do other things
-});
-
-async function loginWithFacebook() {
-  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
-    config.facebook.APP_ID,
-    { permissions: ['public_profile'] }
-  );
-
-  if (type === 'success') {
-    // Build Firebase credential with the Facebook access token.
-    const credential = firebase.auth.FacebookAuthProvider.credential(token);
-
-    // Sign in with credential from the Facebook user.
-    firebase.auth().signInWithCredential(credential).catch((error) => {
-      // Handle Errors here.
-    });
-  }
-}
+import LoginScreen from '../screens/LoginScreen';
 
 // Create the redux store
 const initialState = {};
