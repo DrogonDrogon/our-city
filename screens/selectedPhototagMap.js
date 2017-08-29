2abadd99497a5ad429669b2b6b56bcb8511dcaff
waltershub <waltershub@gmail.com>
2017-08-28 23:39:47 -0400

    updated tagscree

--- screens/selectedPhototagMap.js
+++ screens/selectedPhototagMap.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import PhototagItem from '../components/PhototagItem';
 export default class MapScreen extends React.Component {
   render() {
     console.log('told you so', this.props.navigation.state);
     return (
       <ScrollView>
-        <Text style={styles.titleText} />
         <PhototagItem phototag={this.props.navigation.state.params} />
       </ScrollView>
     );
