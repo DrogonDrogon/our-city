3ad186a39f6585355ea1330d1119a062e6a31e87
Nick <nicholaspowel@users.noreply.github.com>
2017-08-31 09:47:30 -0400

    fuller filter

--- screens/Map/FilterScreen.js
+++ screens/Map/FilterScreen.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import * as Actions from '../../redux/actions';
+import { NavigationActions } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Expo from 'expo';
+import firebase from 'firebase';
+
+class FilterScreen extends Component {
+	
+}
