99e20184465b7d9b63f68d50e2ae599ce7f003c9
Nick <nicholaspowel@users.noreply.github.com>
2017-08-30 21:14:11 -0400

    feat(Maps PhotoTag Filter): Created new FilterScreen file to implement PhotoTag filtering feature
    Created a largely blank file that will be filled with the functions and views required to implment
    filtering on the PhotoTags being displayed in the map and list views
    
    BREAKING CHANGE: no breaking changes
    
    no closed issues

---
+++ screens/Map/FilterScreen.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { ScrollView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import * as Actions from '../../redux/actions';
