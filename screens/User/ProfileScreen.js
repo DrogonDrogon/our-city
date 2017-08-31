140976cc43f162edbe62ab918b5f8bb1d1d7a347
Angela Lin <tigclaw@gmail.com>
2017-08-31 16:27:57 -0400

    Enable allowing user to edit displayName, saves to firebase

--- screens/User/ProfileScreen.js
+++ screens/User/ProfileScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, Button, TouchableHighlight } from 'react-native';
+import { ScrollView, StyleSheet, Text, Button, View, Image, Modal, TextInput } from 'react-native';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import NavigationBar from 'react-native-navbar';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
+import { ImagePicker } from 'expo';
 import firebase from 'firebase';
 import * as Actions from '../../actions';
 import PhototagItem from '../../components/PhototagItem';
@@ -18,8 +21,10 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   // Define the function that will be passed as prop
   return {
     getAllPhototags: () => {
-      console.log('dispatching fetchPhototags');
       dispatch(Actions.fetchPhototags);
+    },
+    submitUserUpdate: userInfo => {
+      dispatch(Actions.updateUser(userInfo));
     },
   };
 };
@@ -54,38 +59,160 @@ class HomeScreen extends React.Component {
     };
   };
 
-  constructor(props) {
-    super(props);
-    this._navigateTo = this._navigateTo.bind(this);
-  }
+  state = {
+    selectedIndex: 0,
+    modalVisibility: false,
+    navBarTitle: {
+      title: 'Edit Profile',
+    },
+    rightButton: {
+      title: 'Save',
+      handler: () => {
+        this._handleSaveProfile();
+      },
+    },
+    leftButton: {
+      title: 'Cancel',
+      handler: () => {
+        this._toggleModal(false);
+      },
+    },
+    editDisplayNameText: this.props.user.displayName,
+    imageUri: this.props.user.photoUrl,
+  };
 
   componentDidMount() {
     this.props.getAllPhototags();
   }
 
-  _navigateTo(routeName) {
+  _handleIndexChange = index => {
+    this.setState({
+      ...this.state,
+      selectedIndex: index,
+    });
+  };
+
+  _navigateTo = routeName => {
     const actionToDispatch = NavigationActions.reset({
       index: 0,
       actions: [NavigationActions.navigate({ routeName })],
     });
     this.props.navigation.dispatch(actionToDispatch);
-  }
-  goTopPhototags(item) {
+  };
+
+  goToPhototags = item => {
     console.log('clicked!!!!!!!!');
     this.props.navigation.navigate('phototagFromUser', item);
-  }
+  };
+
+  _handleClickEdit = () => {
+    console.log('Click edit profile');
+    this.setState({ modalVisibility: true });
+  };
+
+  _pickImage = async () => {
+    console.log('Click change pic');
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true,
+      exif: true,
+    });
+
+    if (!result.cancelled) {
+      this.setState({ imageUri: result.uri });
+      // this.setState({ imageData: result.base64 });
+    }
+  };
+
+  _handleSaveProfile = () => {
+    console.log('Click save profile');
+    // check to see if the name is different, if it's different than make the database call
+    if (
+      this.state.editDisplayNameText !== this.props.user.displayName ||
+      this.state.image !== this.props.user.photoUrl
+    ) {
+      let updatedUser = this.props.user;
+      updatedUser.displayName = this.state.editDisplayNameText;
+
+      console.log('image data uri', this.state.imageUri);
+      // console.log('image data data', this.state.imageData);
+      // let imageFile = this.state.imageData;
+
+      // firebase
+      //   .storage()
+      //   .ref()
+      //   .child(`test/test.png`)
+      //   .putString(imageFile)
+      //   .then(snapshot => {
+      //     console.log('uploaded a base 64 string');
+      //   })
+      //   .catch(err => {
+      //     console.log('problems upload err', err);
+      //   });
+
+      // dispatch saving user
+      this.props.submitUserUpdate(updatedUser);
+    }
+    this._toggleModal(false);
+  };
+
+  _toggleModal = bool => {
+    this.setState({ modalVisibility: bool });
+  };
+
   render() {
     if (this.props.phototags && this.props.user) {
+      let displayName =
+        this.props.user.displayName === '' ? this.props.user.email : this.props.user.displayName;
+
       return (
-        <ScrollView>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.container}>
+            <Image style={styles.profileImage} source={{ uri: this.state.imageUri }} />
+            <Text>{displayName}</Text>
+            <Button title="Edit Profile" onPress={this._handleClickEdit} />
+          </View>
+          <Modal animationType={'slide'} transparent={false} visible={this.state.modalVisibility}>
+            <NavigationBar
+              title={this.state.navBarTitle}
+              rightButton={this.state.rightButton}
+              leftButton={this.state.leftButton}
+            />
+            <ScrollView>
+              <View style={styles.container}>
+                <Image style={styles.profileImage} source={{ uri: this.state.imageUri }} />
+                <Button
+                  title="Change picture"
+                  onPress={this._pickImage}
+                  style={styles.smallButton}
+                />
+                <Text>
+                  Display Name
+                  <TextInput
+                    value={this.state.editDisplayNameText}
+                    keyboardType={'default'}
+                    placeholder="Enter name"
+                    onChangeText={editDisplayNameText => this.setState({ editDisplayNameText })}
+                    style={styles.inputBox}
+                  />
+                </Text>
+              </View>
+            </ScrollView>
+          </Modal>
+          <SegmentedControlTab
+            values={['Posts', 'Starred', 'Comments']}
+            selectedIndex={this.state.selectedIndex}
+            onTabPress={this._handleIndexChange}
+          />
           <Text style={styles.titleText}>Tagged Photos</Text>
           {this.props.phototags
-            .filter(item => item.userName === this.props.user.displayName)
+            .filter(item => item.userId === this.props.user.id)
             .map((item, i) => (
               <PhototagItem
                 phototag={item}
                 key={i}
-                goTophototags={this.goTopPhototags.bind(this, item)}
+                goTophototags={this.goToPhototags.bind(this, item)}
               />
             ))}
         </ScrollView>
@@ -97,16 +224,45 @@ class HomeScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    alignItems: 'center',
+  },
   titleText: {
     textAlign: 'center',
     fontSize: 20,
+    margin: 10,
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  profileName: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  inputBox: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlignVertical: 'top',
+    paddingLeft: 5,
+    paddingRight: 5,
+    width: 100,
+    height: 20,
+    fontSize: 12,
+  },
+  smallButton: {
+    fontSize: 12,
   },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
-//<TouchableHighlight
-//  style={{ width: '100%', height: 200 }}
-//  title=""
-//  key={i}
-//  onPress={this.goTopPhototags.bind(this, item)}
-///>;
