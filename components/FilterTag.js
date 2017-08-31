88fe6827941fadc05341cc1ccbe10776bada9941
Nick <nicholaspowel@users.noreply.github.com>
2017-08-31 15:47:45 -0400

    refactor(FilterScreen and FilterTag): Continue flexhing out visual component
    Making the visual part of the component so that there will be places for data to go
    
    BREAKING CHANGE: none
    
    none

---
+++ components/FilterTag.js
@@ -0,0 +1,18 @@
+import React from 'React';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+
+
+class FilterTagItem extends React.Component {
+
+
+  render(){
+    return(
+      <Button
+        onPress={}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      )
+  }
+}
--- screens/Map/FilterScreen.js
+++ screens/Map/FilterScreen.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import * as Actions from '../../redux/actions';
+import * as Actions from '../../actions';
 import { NavigationActions } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Expo from 'expo';
 import firebase from 'firebase';
+import db from '../../db';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    phototags: state.phototags,
+  };
+};
 
 class FilterScreen extends Component {
-	
+
+state = {
+    comment: '',
+    votes: this.props.navigation.state.params.upvotes,
+    phototag: this.props.navigation.state.params,
+    edited: false,
+    comments: this.props.navigation.state.params.comments,
+  };
+
+  
+  render(){
+    return(
+      <ScrollView>
+        <ScrollView>
+        {this.state.comments.map((comment, i) => (
+            <Comment key={i} userName={this.state.phototag.userName} comment={comment} />
+          ))}
+        </ScrollView>
+        <Slider>
+        </Slider>
+        <Slider>
+        </Slider>
+        <View>
+        </View>
+      </ScrollView>
+      )
+  }
 }
+
+export default FilterScreen;
+
+//need an option for favorites
+//need a selector for recent, popular, most voted, most faved
+//need to allow selection by tag (check marks?)
+//allow for different zoom levels?
+//change search radius
