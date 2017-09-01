8173bccc623644ac017723c9f80f35f623499fef
waltershub <waltershub@gmail.com>
2017-09-01 12:51:04 -0400

    added location raduis method

--- screens/Map/MapScreen.js
+++ screens/Map/MapScreen.js
@@ -14,17 +14,21 @@ const mapStateToProps = (state, ownProps) => {
 class MapScreen extends React.Component {
   state = {
     region: {
-      latitude: 20.750355960509054,
+      latitude: 40.750355960509054,
       longitude: -73.97669815393424,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     },
+    markers: {},
   };
 
   componentWillMount() {
     this.getLocation();
+    this.setState({ markers: this.props.phototags });
+    //this.setMarkersByDistance();
     Location.watchPositionAsync(location => {
       this.setLocation(location);
+      //this.setMarkersByDistance();
     });
   }
 
@@ -55,6 +59,44 @@ class MapScreen extends React.Component {
     };
     console.log(tempRegion);
     this.setState({ region: tempRegion });
+  }
+
+  checkDistance(
+    lat1,
+    lon1,
+    lat2 = this.state.region.latitude,
+    lon2 = this.state.region.longitude,
+    distance = 1000
+  ) {
+    Number.prototype.toRadians = () => {
+      return this * Math.PI / 180;
+    };
+    var R = 6371e3; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2 - lat1).toRadians();
+    var Δλ = (lon2 - lon1).toRadians();
+
+    var a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    var d = R * c;
+    //if (d > distance) return false;
+    return true;
+  }
+
+  setMarkersByDistance() {
+    var tempMarkers = [];
+    this.state.markers.forEach(marker => {
+      if (this.checkDistance(marker.locationLat, marker.locationLong)) {
+        tempMarkers.push(marker);
+      }
+    });
+    this.setState({ markers: tempMarkers }, () => {
+      console.log('markers', this.state.markers);
+    });
   }
 
   render() {
