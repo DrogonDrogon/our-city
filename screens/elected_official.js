cf89af52a49fda44a9ffa7dd7265f2d37f67cc4f
waltershub <waltershub@gmail.com>
2017-09-07 14:07:46 -0400

    feat(simolify): simpligied electied officails
    simplfied
    
    BREAKING CHANGE: none
    
    noe

--- screens/elected_official.js
+++ screens/elected_official.js
@@ -12,6 +12,10 @@ import {
   Picker,
 } from 'react-native';
 export default class electedOfficails extends React.Component {
+  state = {
+    electedOfficailIndex: this.props.navigation.state.params.electedOfficailIndex,
+    officials: this.props.navigation.state.params.reps.officials,
+  };
   render() {
     console.log('electedOfficails', this.props.navigation.state.params.electedOfficailIndex);
     return (
@@ -19,9 +23,7 @@ export default class electedOfficails extends React.Component {
         <Image
           style={{ width: '100%', height: 100, resizeMode: Image.resizeMode.contain }}
           source={{
-            uri: this.props.navigation.state.params.reps.officials[
-              this.props.navigation.state.params.electedOfficailIndex
-            ].photoUrl,
+            uri: this.state.officials[this.state.electedOfficailIndex].photoUrl,
           }}
         />
       </View>
