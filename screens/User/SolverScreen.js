872eda72aa3c875823ce31add207ce22b9b111b3
Nick <nicholaspowel@users.noreply.github.com>
2017-09-11 11:51:45 -0400

    SolverScreen template

---
+++ screens/User/SolverScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import moment from 'moment';
+import { connect } from 'react-redux';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Share,
+  Modal,
+} from 'react-native';
+import NavigationBar from 'react-native-navbar';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Ionicons } from '@expo/vector-icons';
+import * as Actions from '../../actions';
+import db from '../../db';
+
+class SolverScreen extends React.Component {
+  state = {}
+
+}
+
+export default SolverScreen;
