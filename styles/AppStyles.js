30eb3434459d6eb04261eb1bd15b6d100d873e70
Nick <nicholaspowel@users.noreply.github.com>
2017-09-14 15:17:57 -0400

    modded

--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Image } from 'react-native';
 
+//GLOBAL COLORS
+const dark = '#eee';
+//-----------
+
 const AppStyles = StyleSheet.create({
+
   splash: {
     height: '100%',
     width: '100%',
@@ -48,10 +53,7 @@ const AppStyles = StyleSheet.create({
     marginRight: 10,
     borderRadius: 20,
   },
-  hashtag: {
-    color: 'blue',
-    fontWeight: 'bold',
-  },
+
   iconStyle: {
     backgroundColor: '#FBF8F5',
   },
@@ -99,7 +101,11 @@ const AppStyles = StyleSheet.create({
     height: 50,
     margin: 6,
     borderRadius: 25,
-  }
+  },
+  hashtag: {
+    color: 'blue',
+    fontWeight: 'bold',
+  },
 });
 
 export default AppStyles;
